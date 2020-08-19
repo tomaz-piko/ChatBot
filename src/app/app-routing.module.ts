b14776cb4d4edeeadc516744f01e033ab7ca847a
Tomaz Piko <tomaz.piko@gmail.com>
2020-08-19 16:57:54 +0200

    Pushing app files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
+import { ChatBoxEngComponent } from './chat-box-eng/chat-box-eng.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ChatBoxComponent },
+  {path: 'ChatBoxEng', component: ChatBoxEngComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ChatBotB'`, () => {
+  it(`should have as title 'ChatBot2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ChatBotB');
+    expect(app.title).toEqual('ChatBot2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ChatBotB app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ChatBot2 app is running!');
   });
 });
